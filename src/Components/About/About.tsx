938fc3311088520fc03cce34aba86c8d69ce781f
Benedikt89 <constantine.kostka@gmail.com>
2020-01-31 05:07:31 +0300

    some about UI and datePicker practice

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,41 +1,143 @@
-import React, {useState} from 'react';
-import slide from "./../../assets/images/slide1.png"
-import Preloader from "../../common/Preloader";
+import React, {Component} from 'react';
 import {fetchOrders} from "../../Redux/productsReducer";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import style from './About.module.css';
 import {AppStateType} from "../../Redux/Store";
+import axios from "axios";
+import Slider from "../../common/Slider";
+import bgPict from "../../assets/images/slide1.png";
+// @ts-ignore
+import {Fade} from "react-reveal";
+import {DatepickerRU} from "../../common/FormControls/DatePicker";
 
 interface IProps {
-
+    filters: Array<any>,
+    pizzas: Array<any>
 }
 
-const About: React.FC = () => {
+interface IState {
+    imageLoaded: boolean
+    order: any
+}
 
-    let [imageLoaded, handleImageLoaded] = useState(false);
+interface I_dispatchProps {
+    fetchOrders: () => void
+}
 
-    return (
-        <div className={style.aboutWrapper}>
-            <div>
-                {!imageLoaded && <Preloader/>}
-                <img src={slide} onLoad={() => {
-                    handleImageLoaded(true)
-                }} alt={"Pechorin Bulki"}/>
-            </div>
-            <article>
-                fsad;ljg sdf soiuhdf isudfh lksufh lskduf lskdf hsdhf lius\
-                fsdj gsuhi shdf sdf
-                sdfg nsfug sudfn
-                sfgh sgijh
-            </article>
-        </div>
-    );
+let commonCarusel = {
+    index: {
+        "front_image": [
+            {"image": bgPict},
+            {"image": bgPict},
+            {"image": bgPict}
+        ],
+        front_text: [
+            {text: "carousel asdasdasd image 1"},
+            {text: "Общество с ограниченной ответственностью «Печь Орин» image 2"},
+            {text: "Общество с ограниченной ответственностью «Печь Орин» image 3"}
+        ]
+    }
 };
 
+class About extends Component<IProps & I_dispatchProps & IState> {
+    state: IState = {
+        imageLoaded: false,
+        order: {
+            phone: '222333111',
+            first_name: 'string',
+            delivery_date: '2000-10-21',
+            delivery_time: 2,
+            address: 'string',
+            comment: 'string',
+            payment: 0,
+            order_items: [
+                {
+                    quantity: 3,
+                    pizza: 1,
+                }, {
+                    quantity: 2,
+                    pizza: 2,
+                }
+            ]
+        }
+    };
+
+    postOrder = () => {
+        console.log(this.state.order);
+        axios.post("http://127.0.0.1:8000/api/order/", this.state.order)
+            .then(res => {
+                alert(res.request)
+            })
+            .catch(err => {
+                alert(err)
+            })
+
+    };
+
+    handleImageLoaded() {
+        this.setState({imageLoaded: true});
+    }
+
+    render() {
+
+        return (
+            <div>
+                <Slider
+                    commonImages={commonCarusel.index.front_image}
+                    commonTexts={commonCarusel.index.front_text}
+                />
+                <div className={style.aboutWrapper}>
+                    <Fade cascade>
+                        <div className={style.row}>
+                            <div className={style.col5}>
+                                <img src={'https://pechorin.by/media/hardcode/about/5.jpg'}/>
+                            </div>
+                            <div className={style.col7}>
+                                <h3>О сервисе</h3>
+                                <article>Мы работаем с понедельника по пятницу, по рабочим дням. Заказы сегодня на
+                                    сегодня принимаются только по телефону до 12-30. Время для приготовления и доставки
+                                    занимает от 1,5 часов, и зависит от величины заказа и района доставки. Если Вам
+                                    нужно приготовить большой заказ или важно время доставки, пожалуйста, сделайте заказ
+                                    заранее.
+                                </article>
+                            </div>
+                        </div>
+                        <div className={style.row}>
+                            <div className={style.col5}>
+                                <img src={'https://pechorin.by/media/hardcode/about/5.jpg'}/>
+                            </div>
+                            <div className={style.col7}>
+                                <div className={style.container}>
+                                    <h3>О сервисе</h3>
+
+                                    <article>Мы работаем с понедельника по пятницу, по рабочим дням. Заказы сегодня на
+                                        сегодня принимаются только по телефону до 12-30. Время для приготовления и
+                                        доставки
+                                        занимает от 1,5 часов, и зависит от величины заказа и района доставки. Если Вам
+                                        нужно приготовить большой заказ или важно время доставки, пожалуйста, сделайте
+                                        заказ
+                                        заранее.
+                                    </article>
+                                </div>
+                            </div>
+                        </div>
+                    </Fade>
+                    <div>
+                        <DatepickerRU />
+                    </div>
+                </div>
+                <button onClick={this.postOrder}>post test order</button>
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = (state: AppStateType) => {
-    return {}
+    return {
+        filters: state.reducer.filters,
+        pizzas: state.reducer.products
+    }
 };
 
 export default compose(connect(mapStateToProps, {fetchOrders}))(About);
--- src/common/Slider.tsx
+++ src/common/Slider.tsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Carousel} from "antd";
 import 'antd/dist/antd.css'
 import './antdComponents.css'
-
+import Preloader from "./Preloader";
 
 interface I_props {
-    commonImages: Array<{ image_name: string, image: string }>
-    commonTexts: Array<{ text_name: string, text: string }>
+    commonImages: Array<{ image_name?: string, image: string }>
+    commonTexts: Array<{ text_name?: string, text: string }>
 }
 
 let Slider = ({commonImages, commonTexts}: I_props) => {
+    let [imageLoaded, handleImageLoaded] = useState(false);
+
     let items = commonImages.map((item, index) => {
         return (
-            <div>
+            <div key={item.image_name ? item.image_name : 'catousel'+index }>
+                {/*{!imageLoaded && <Preloader/>}*/}
+                {/*<img src={slide} onLoad={() => {handleImageLoaded(true)}} alt={"Pechorin Bulki"}/>*/}
                 <div style={{
-                    backgroundImage: `${item.image})`,
+                    backgroundImage: `url(http://127.0.0.1:8000${item.image})`,
                     backgroundPosition: 'center center',
                     backgroundRepeat: 'no-repeat',
                     height: `35rem`,
                     backgroundSize: 'cover',
-                }} className="caruselContent">
-                    <h3>
-                        {commonTexts[index].text}
-                    </h3>
+                }}className="caruselContent">
+                        <h3>
+                            {commonTexts[index].text}
+                        </h3>
                 </div>
             </div>
         )
